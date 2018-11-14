fe1f2382f98bc0df0f21a67e4a90d79f7589ce1e
Harrison Brock <harrison@harrisonbrock.com>
2018-11-14 11:17:25 -0500

    Create a Menu and Popup to show credits

--- app/src/main/java/com/example/harrisonbrock/timefighter/GameActivity.kt
+++ app/src/main/java/com/example/harrisonbrock/timefighter/GameActivity.kt
@@ -3,7 +3,10 @@ package com.example.harrisonbrock.timefighter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.support.v7.app.AlertDialog
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.animation.AnimationUtils
 import android.widget.Button
 import android.widget.TextView
@@ -53,6 +56,32 @@ class GameActivity : AppCompatActivity() {
            resetGame()
        }
         
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_settings) {
+            showInfo()
+        }
+
+        return true
+    }
+
+    private fun showInfo() {
+
+        val dialogTitle = getString(R.string.about_title, BuildConfig.VERSION_NAME)
+        val dialogMessage = getString(R.string.about_message)
+
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(dialogTitle)
+        builder.setMessage(dialogMessage)
+        builder.create().show()
+
     }
 
     private fun restoreGame() {
