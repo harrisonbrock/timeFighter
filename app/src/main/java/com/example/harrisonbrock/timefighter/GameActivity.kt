793248b5f5a4a8351c0f46308088c359e89dbc13
Harrison Brock <harrison@harrisonbrock.com>
2018-11-13 23:29:52 -0500

    Fixed bug with rotation

--- app/src/main/java/com/example/harrisonbrock/timefighter/GameActivity.kt
+++ app/src/main/java/com/example/harrisonbrock/timefighter/GameActivity.kt
@@ -3,11 +3,14 @@ package com.example.harrisonbrock.timefighter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
 
 class GameActivity : AppCompatActivity() {
+
+    internal val TAG = GameActivity::class.java.simpleName
 
     internal lateinit var gameScoreTextView: TextView
     internal lateinit var timeLeftTextView: TextView
@@ -20,7 +23,13 @@ class GameActivity : AppCompatActivity() {
     internal var countDownInterval: Long = 1000
     internal var timeleft = 60
 
+    companion object {
+        private val SCORE_KEY = "SCORE_KEY"
+        private val TIME_LEFT_KEY = "TIME_LEFT_KEY"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG, "onCreate called. Score is: $score")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         gameScoreTextView = findViewById(R.id.game_score_text_view)
@@ -29,8 +38,56 @@ class GameActivity : AppCompatActivity() {
         
         tapMeButton.setOnClickListener { v ->  incrementScore() }
 
-        resetGame()
+       if (savedInstanceState != null) {
+           score = savedInstanceState.getInt(SCORE_KEY)
+           timeleft = savedInstanceState.getInt(TIME_LEFT_KEY)
+           restoreGame()
+       } else {
+           resetGame()
+       }
         
+    }
+
+    private fun restoreGame() {
+        val restoredScore = getString(R.string.your_score, Integer.toString(score))
+        gameScoreTextView.text = restoredScore
+
+        val restoredTime = getString(R.string.time_left, Integer.toString(timeleft))
+        timeLeftTextView.text = restoredTime
+
+        countDownTimer = object : CountDownTimer((timeleft * 1000).toLong(), countDownInterval) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                timeleft = millisUntilFinished.toInt() / 1000
+
+                val timeLeftString = getString(R.string.time_left, Integer.toString(timeleft))
+                timeLeftTextView.text = timeLeftString
+            }
+
+            override fun onFinish() {
+                endGame()
+            }
+
+
+        }
+        countDownTimer.start()
+        gameStarted = true
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt(SCORE_KEY, score)
+        outState.putInt(TIME_LEFT_KEY, timeleft)
+        countDownTimer.cancel()
+
+        Log.d(TAG, "OnSaveInstanceState: Saving Score: $score & Time Left: $timeleft")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy call.")
     }
 
     private fun incrementScore() {
